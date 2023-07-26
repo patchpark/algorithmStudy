3d79598773765209fcdfe243b95c187f19f103e1
patchpark <kevinminsang99@gmail.com>
2023-07-26 19:59:30 +0900

    400 타일링

---
+++ 알고리즘 기초 1/400(다이나믹 프로그래밍1)/11726/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/11726
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int[] arr = new int[1001];
+		arr[1] = 1;
+		arr[2] = 2;
+		
+		for(int i=3; i<=num; i++) {
+			arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+		}
+		
+		System.out.println(arr[num]);
+		
+	}
+	
+}
