964bf8e2308b295788d4edadc31843ffdb98d5d7
patchpark <kevinminsang99@gmail.com>
2023-07-30 22:38:13 +0900

    기초1 400 카드 구매 1,2

---
+++ 알고리즘 기초 1/400(다이나믹 프로그래밍1)/11052/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/11052
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		int arr[] = new int[num+1];
+		
+		
+		for(int i=1; i<=num; i++) {
+			int temp = sc.nextInt();
+			arr[i] = temp;
+			for(int j=1; j<=i; j++) {
+				if(temp <= arr[i-j]+arr[j]) {
+					temp =  arr[i-j]+arr[j];
+				}
+			}
+			arr[i] = temp;
+			
+		}
+		System.out.println(arr[num]);
+		
+	}
+	
+}
