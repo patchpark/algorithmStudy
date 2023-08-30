9310cb355721c436353e830a7dc303b732427c82
patchpark <kevinminsang99@gmail.com>
2023-08-30 20:34:41 +0900

    기초2 510 15654, 15655, 15656, 15657

---
+++ 알고리즘 기초 2/510(브루트 포스 N과 M)/15657/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/15657
+public class Main {
+	public static int N;
+	public static int M;
+	public static int arr[];
+	public static boolean check[];
+	public static int arr2[];
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		arr = new int[N];
+		arr2 = new int[M];
+		check = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		dfs(0, 0);
+
+		System.out.println(sb);
+	}
+
+	public static void dfs(int start, int depth) {
+		if (depth == M) {
+			for (int i : arr2) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			arr2[depth] = arr[i];
+			dfs(i, depth+1);
+		}
+	}
+}
